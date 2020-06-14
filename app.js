0fa43b2165457aab98ace59c223352a2df9b2192
hassan-bin-shahbaz <hassanshahbaz992@gmail.com>
2020-06-14 17:41:10 +0500

    practise git

---
+++ app.js
@@ -0,0 +1,236 @@
+// // Chapter1
+// // Task 1
+// alert("hello there!")
+
+// // Task 2
+// alert("Error! Please enter valid password")
+
+// // Task 3
+// alert("Welcome to JS Land"+"\n"+"Happy Coding!")
+
+// // Task 4
+// alert("Error! Please enter valid password")
+// alert("Welcome to JS Land"+"\n"+"Happy Coding!")
+
+// //  Task 5
+// console.log("Hello... Ican run JS through my browser's console")
+
+// // Chapter 2
+// // Task1
+// var username
+
+// //Task 2
+// var myName;
+// myName="Hassan bin Shahbz"
+
+// // // Task3
+// var message;
+// message="Hello World";
+// alert(message)
+
+// // Task 4
+// var name,age,courseName;
+// name="jhone Doe";
+// age=15;
+// courseName="certified Mobile Application Development";
+// alert(name);
+// alert(age+"years old");
+// alert(courseName);
+
+// //Task 5
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// //Task 6
+// var email="example@example.com";
+// alert("My email address is "+email);
+
+// //Task 7
+// var book="A smarter way to learn JavaScript"
+// alert("I'am trying to learn from book "+book)
+
+// //Task 8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// // //Task 9
+// alert('▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬')
+
+// // Chapter 3
+// Task 1
+// var age;
+// age=21;
+// alert("I'am "+age+" years old.");
+
+// // Task2
+// var N;
+// N=14;
+// alert("You visited this page "+N+" times")
+
+// // Task 3
+// var birthYear;
+// birthYear=1999;
+// document.write("My birth year is "+birthYear+'<br>'+"data type of declayred variable is "+typeof(birthYear));
+
+// //Task 4
+// var visitorName,productTitle,Quantity;
+// visitorName="John Doe";
+// productTitle="Tshirts";
+// Quantity=5
+// document.write(visitorName+" ordered "+ Quantity +productTitle+" on XYZ Clothing store");
+
+// // Chapter 4
+// // Task 1
+// var one,two,three;
+
+// // Task 2
+// //Legal
+// var a,b,c;
+// //Illegal
+// var !wqw,typeof,list;
+
+// // task 3
+// document.write("<h1>Rules for naming JS variables</h1>"+'<br>')
+// document.write("Variable names can only contain numbers, $ and _ ."+'<br>')
+// document.write("Variables must begin with a letter, $ or_. For example $name, _name or name"+'<br>')
+// document.write("Variable names are case sensitive"+'<br>')
+// document.write("Variable names should not be JS keywords"+'<br>')
+
+// // Chapter 5
+// // // Task1
+// var one , two , sum;
+// one=3;
+// two=5;
+// sum=one+two;
+// document.write("Sum of "+one+" and "+two+" is " +sum);
+
+// // Task2
+
+// var one , two , sum;
+// one=3;
+// two=5;
+// sum=one-two;
+// document.write("Subtaction of "+one+" and "+two+" is " +sum);
+
+
+// var one , two , sum;
+// one=3;
+// two=5;
+// sum=one*two;
+// document.write("Multiple of "+one+" and "+two+" is " +sum);
+
+
+// var one , two , sum;
+// one=3;
+// two=5;
+// sum=one/two;
+// document.write("Division of "+one+" and "+two+" is " +sum);
+
+
+// var one , two , sum;
+// one=3;
+// two=5;
+// sum=one%two;
+// document.write("Mod of "+one+" and "+two+" is " +sum);
+
+// // Task3
+// var a;
+// document.write("the value of declayerd vairable is "+a+"<br>");
+// a=1
+// document.write("THe initial value is "+a+'<br>');
+// a++
+// document.write("THe incremented value is "+a+'<br>');
+// a=a+7
+// document.write("THe additional value is "+a+'<br>');
+// a=a-1
+// document.write("THe decrement value is "+a+'<br>');
+// a=a%3
+// document.write("THe remainder after dividing by 3 is "+a+'<br>');
+
+// // Task 4'
+// ticket=600;
+// ticket=ticket*5;
+// document.write("Total cost of buy 5 tickets to a movie is "+ticket+"PKR");
+
+// // // Task5
+// var table;
+// table=4
+// document.write("table of "+table);
+// document.write(table+"x 1 = "+table+"<br>");
+// document.write(table+"x 2 = "+table*2+"<br>");
+// document.write(table+"x 3 = "+table*3+"<br>");
+// document.write(table+"x 4 = "+table*4+"<br>");
+// document.write(table+"x 5 = "+table*5+"<br>");
+// document.write(table+"x 6 = "+table*6+"<br>");
+// document.write(table+"x 7 = "+table*7+"<br>");
+// document.write(table+"x 8 = "+table*8+"<br>");
+// document.write(table+"x 9 = "+table*9+"<br>");
+// document.write(table+"x 10 = "+table*10+"<br>");
+
+// // task 6
+// var centi,fahren;
+// centi=25;
+// fahren=77;
+// fahren= (centi * 9/5) + 32;
+// centi= (fahren-32) * 5/9;
+// document.write(centi+"℃ "+"is "+fahren+"℉"+"<br>");
+// document.write(fahren+"℉"+" is "+centi+"℃ ");
+
+// // Task 7
+// var item1,item2,oitem1,oitem2,scharges;
+// item1=650
+// qitem1=3
+// item2=100
+// qitem2=7
+// scharges=100
+// total=item1*qitem1+item2*qitem2+scharges;
+// document.write("total cost of order is "+total);
+
+// // task 8
+// var tm,mo;
+// tm=980;
+// mo=804;
+// per=mo*100/tm;
+// document.write("total marks:"+tm+"<br>");
+// document.write("Obtained marks:"+mo+"<br>");
+// document.write("Precenteage:"+per+"%"+"<br>");
+
+// // Task 9
+// Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee and 1 Saudi Riyal = 28 Pakistani Rupee
+// var usd,saudiRiyal,total;
+// usd=10;
+// saudiRiyal=25;
+// usd=104.80*usd;
+// saudiRiyal=28*saudiRiyal;
+// total=usd+saudiRiyal;
+// document.write("Total Currency is PKR: "+ total);
+
+// // Task 10
+// var num;
+// num=1
+// num = [(num+5)*10]/2
+// document.write(num)
+
+// // Task 11
+// currentYear=2020;
+// birthYear=1999;
+// yourAge=currentYear-birthYear;
+// document.write("Your age is :"+yourAge);
+
+// // Task 12 
+// var radius=20;
+// // Circumference of a circle = 2 π r , π = 3.142
+// var circ=2*3.142*radius;
+// // Area of a circle = π r2, π = 3.142
+// var area=3.142*(radius*radius)
+// document.write("Radius:"+radius+'<br>'+"Circumference:"+circ+'<br>'+"Area:"+area)
+
+// // Task 13
+// var favSnack="lays"
+// var curAge=15
+// var estAmt=65
+// var snackPerDay=3
+// var total=estAmt-curAge
+// var need=total*snackPerDay
+// document.write("You will need " + need + " to last you until the ripe old age of " + estAmt)
+
+// // Chapter 6-9
+// // Task 1
